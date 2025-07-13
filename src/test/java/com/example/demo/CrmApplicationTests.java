54dcaae3bbe241f706e83b4b9bf9dea5dfafabcd
Egor Zhukov <zhukoven@ssp-soft.com>
2025-07-13 20:34:52 +0300

    fix errors

--- src/test/java/com/example/demo/CrmApplicationTests.java
+++ src/test/java/com/example/demo/CrmApplicationTests.java
@@ -3,13 +3,9 @@ package com.example.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
 @ActiveProfiles("test")
-@TestPropertySource(properties = {
-		"spring.liquibase.enabled=false"
-})
 class CrmApplicationTests {
 
 	@Test
